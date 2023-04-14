f571a290c65a78eae5635c787b9971478d0612f4
kommineni469 <123137091+kommineni469@users.noreply.github.com>
2023-04-13 23:11:14 -0500

    UPDATED

--- controllers/car.js
+++ controllers/car.js
@@ -1,8 +1,18 @@
 var Car = require('../models/car');
 // List of all cars
-exports.car_list = function(req, res) {
- res.send('NOT IMPLEMENTED: car list');
-};
+exports.car_list = async function(req, res) {
+    try{
+    theCars = await Car.find();
+    res.send(theCars);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+   };
+// exports.car_list = function(req, res) {
+//  res.send('NOT IMPLEMENTED: car list');
+// };
 // for a specific car.
 exports.car_detail = function(req, res) {
  res.send('NOT IMPLEMENTED: car detail: ' + req.params.id);
@@ -19,3 +29,48 @@ exports.car_delete = function(req, res) {
 exports.car_update_put = function(req, res) {
  res.send('NOT IMPLEMENTED: car update PUT' + req.params.id);
 };
+// List of all cars
+exports.car_list = async function(req, res) {
+try{
+thecars = await Car.find();
+res.send(thecars);
+}
+catch(err){
+res.status(500);
+res.send(`{"error": ${err}}`);
+}
+};
+// VIEWS
+// Handle a show all view
+exports.car_view_all_Page = async function(req, res) {
+ try{
+ thecars = await Car.find();
+ res.render('car', { title: 'car Search Results', results: thecars });
+ }
+ catch(err){
+ res.status(500);
+ res.send(`{"error": ${err}}`);
+ }
+};
+// Handle Costume create on POST.
+exports.car_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new Car();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.carName = req.body.carName;
+    document.carcolor = req.body.carcolor;
+    document.carcount = req.body.carcount;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
+    
+
--- routes/car.js
+++ routes/car.js
@@ -1,9 +1,15 @@
+// var express = require('express');
+// var router = express.Router();
+
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('car', { title: 'Search Results by car' });
+// });
+
+// module.exports = router;
 var express = require('express');
+const car_controlers= require('../controllers/car');
 var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('car', { title: 'Search Results by car' });
-});
-
+/* GET cars */
+router.get('/', car_controlers.car_view_all_Page );
 module.exports = router;
